16bffc39947c9ee085460f2cf90757440d079b34
Ian <145183497+2anizirong@users.noreply.github.com>
2024-06-22 00:27:13 +0900

    Create C++0005.cpp

---
+++ C++/C++0005.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x, y, w, h;
+    cin>>x>>y>>w>>h;
+    
+    int min1=min(x,y);
+    int min2=min(min1, w-x);
+    int min3=min(min2,h-y);
+    
+    cout<<min3<<endl;
+
+    return 0;
+}

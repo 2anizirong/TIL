e50842f90a170b9230b315386e98154fa6fbe3f1
Ian <145183497+2anizirong@users.noreply.github.com>
2024-06-26 16:33:52 +0900

    Create J0010.java

---
+++ JAVA/J0010.java
@@ -0,0 +1,39 @@
+// 1362
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 1;
+
+        while (true) {
+            int o = sc.nextInt();
+            int w = sc.nextInt();
+
+            if (o == 0 && w == 0) break;
+
+            boolean ok = true;
+            char patAction;
+            int n;
+
+            while (true) {
+                patAction = sc.next().charAt(0);
+                n = sc.nextInt();
+
+                if (patAction == '#' && n == 0) break;
+                if (patAction == 'F') w += n;
+                else if (patAction == 'E') w -= n;
+
+                if (w <= 0) ok = false;
+            }
+
+            if (ok) {
+                if (w > (o /2) && w < (o * 2)) System.out.println(cnt + " :-)"); 
+                else System.out.println(cnt + " :-(");
+            } 
+            else System.out.println(cnt + " RIP");
+            cnt++;
+        }
+    }
+}

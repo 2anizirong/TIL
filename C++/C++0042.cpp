351a0f205296a1922e3cc76a9b76a521758853fc
Ian <145183497+2anizirong@users.noreply.github.com>
2024-07-17 01:38:20 +0900

    Create C++0042.cpp

---
+++ C++/C++0042.cpp
@@ -0,0 +1,20 @@
+// 10870
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int *fibo = new int[n + 1];
+    fibo[0] = 0; fibo[1] = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    cout << fibo[n] << '\n';
+
+    return 0;
+}
